d1a5f8228252b999e04d94804e5e2785cfceaeba
nugosviandze <nugzar.svianadze.1@btu.edu.ge>
2025-05-21 20:12:44 +0400

    sqlalchemy database uri default

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "my_secret_key")
-    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URL", "sqlite:///my_first_db.sqlite3")
     UPLOAD_FOLDER = 'uploads/'
