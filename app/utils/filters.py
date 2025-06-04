9d273cfea7dc1cde1eef9da962c31cdcfa6a1671
nugosviandze <nugzar.svianadze.1@btu.edu.ge>
2025-06-04 20:02:45 +0400

    flask sessions, password hashing and structurisation

---
+++ app/utils/filters.py
@@ -0,0 +1,7 @@
+# @app.template_filter('remove_numbers')
+# def remove_numbers(s):
+#     final_string = ""
+#     for letter in s:
+#         if not letter.isnumeric():
+#             final_string += letter
+#     return final_string
